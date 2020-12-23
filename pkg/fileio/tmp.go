9063344456d94fdf1ff51968f46da133617be6e8
Rohan Mathew <robozmat@gmail.com>
2020-12-23 19:53:34 +0000

    removed .idea and corrected scraping

--- pkg/fileio/tmp.go
+++ pkg/fileio/tmp.go
@@ -1,6 +1,7 @@
 package fileio
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -8,17 +9,25 @@ import (
 	"strings"
 )
 
-var TempDir string
+var tmpDir string
 
-func initialize(dir, pattern string) {
-	TempDir, _ = ioutil.TempDir(dir, pattern)
+func Init(dir, pattern string) {
+	var err error
+	tmpDir, err = ioutil.TempDir(dir, pattern)
+	if err != nil {
+		rd := bufio.NewScanner(os.Stdin)
+		fmt.Println("Unable to create temp dir")
+		fmt.Print("Enter new temp dir: ")
+		rd.Scan()
+		tmpDir = rd.Text()
+	}
 }
 
-func TempPath(path string) (string, error) {
+func TmpPath(path string) (string, error) {
 	// Make chain of paths
 	pthSplit := strings.Split(path, string(filepath.Separator))
 	totPath := make([]string, len(pthSplit)+1)
-	totPath[0] = TempDir
+	totPath[0] = tmpDir
 	for i := 0; i < len(pthSplit); i++ {
 		totPath[i+1] = pthSplit[i]
 	}
