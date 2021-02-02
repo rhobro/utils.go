652f2a2a7fa0ec2b91ce60052c8b4e9aef32d7a4
Rohan Mathew <robozmat@gmail.com>
2021-02-02 15:14:02 +0000

    added verbose func

--- pkg/cfgcat/configcat.go
+++ pkg/cfgcat/configcat.go
@@ -5,21 +5,28 @@
  *
  * Project: goutils
  * File Name: configcat.go
- * Last Modified: 02/02/2021, 09:57
+ * Last Modified: 02/02/2021, 15:13
  */
 
 package cfgcat
 
 import (
 	"github.com/configcat/go-sdk/v7"
+	"log"
 )
 
 var C *configcat.Client
 
-func Init(key string) {
+func Init(key string, verbose bool) {
 	C = configcat.NewClient(key)
+	if verbose {
+		log.Print("{configcat} connected")
+	}
 }
 
-func InitCustom(cfg configcat.Config) {
+func InitCustom(cfg configcat.Config, verbose bool) {
 	C = configcat.NewCustomClient(cfg)
+	if verbose {
+		log.Print("{configcat} connected")
+	}
 }
