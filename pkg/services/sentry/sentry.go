faf6e1bb0c7c034b44708d3b0d3bad3cb78c6c6d
Rohan Mathew <robozmat@gmail.com>
2021-02-03 20:41:28 +0000

    sentry implementation

---
+++ pkg/services/sentry/sentry.go
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2021 NeuroByte Tech. All rights reserved.
+ *
+ * NeuroByte Tech is the Developer Company of Rohan Mathew.
+ *
+ * Project: goutils
+ * File Name: sentry.go
+ * Last Modified: 03/02/2021, 20:37
+ */
+
+package sentry
+
+import (
+	"github.com/getsentry/sentry-go"
+	"log"
+)
+
+var C *sentry.Client
+
+func Init(opt sentry.ClientOptions, verbose bool) {
+	var err error
+	C, err = sentry.NewClient(opt)
+	if err != nil {
+		log.Fatalf("can't connect to sentry: %s", err)
+	}
+
+	if verbose {
+		log.Print("{sentry} connected")
+	}
+}
