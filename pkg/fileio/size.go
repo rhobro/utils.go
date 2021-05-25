646419397b6d8f33a4147e2521ebc281f02b2e22
Rohan Mathew <robozmat@gmail.com>
2021-05-25 21:00:47 +0100

    dir size

---
+++ pkg/fileio/size.go
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2021 NeuroByte Tech. All rights reserved.
+ *
+ * NeuroByte Tech is the Developer Company of Rohan Mathew.
+ *
+ * Project: goutils
+ * File Name: size.go
+ * Last Modified: 25/05/2021, 21:00
+ */
+
+package fileio
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func DirSize(path string) (int64, error) {
+	var size int64
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return err
+	})
+	return size, err
+}
