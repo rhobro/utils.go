85fb4171f7ba5e647c55967ca13e5dfd7fab7aa2
rhobro <robozmat@gmail.com>
2021-08-04 14:49:56 +0000

    asdf

--- pkg/services/mongrel/mongo.go
+++ pkg/services/mongrel/mongo.go
@@ -2,7 +2,6 @@ package mongrel
 
 import (
 	"context"
-	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -11,10 +10,8 @@ import (
 var C *mongo.Client
 
 func Init(url string) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 20 * time.Second)
-	defer cancel()
 	var err error
-	C, err = mongo.Connect(ctx, options.Client().ApplyURI(url))
+	C, err = mongo.Connect(context.Background(), options.Client().ApplyURI(url))
 	if err != nil {
 		return err
 	}
